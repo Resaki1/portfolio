27e566265331694617d5cd2bc4ce488b19b34913
Resaki1 <christian.reski@t-online.de>
2024-06-29 18:49:34 +0200

    styling and fonts setup

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>
+        Hi, I'm Christian <br />
+        Web Developer
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
