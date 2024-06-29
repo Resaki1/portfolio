523453266c377ada3a725536a279183b6d7b38e8
Resaki1 <christian.reski@t-online.de>
2024-06-29 21:06:02 +0200

    add about page

---
+++ src/components/About/About.tsx
@@ -0,0 +1,29 @@
+import "./About.scss";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>
+        Hello there! My name is Christian, I'm a web developer from Karlsruhe,
+        Germany.
+      </p>
+      <p>
+        My love for web development sparked in 2019, when I started an
+        internship. Since then, I have been a working student in that field.
+        Additionally, I started freelancing as a web developer in 2021.
+      </p>
+      <p>
+        I'm enthusiastic about working with React, Vue, and Astro. I love
+        working in teams (especially agile ones), and also like doing some UI/UX
+        design here and there.
+      </p>
+      <p>
+        In my free time, you will see me hiking, playing (table) tennis,
+        photographing distant galaxies, or playing the piano.
+      </p>
+    </section>
+  );
+};
+
+export default About;
